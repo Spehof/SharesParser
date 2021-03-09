b2fb96b35d5e27a1b8c238b582bca161db1914c4
Roman Volkov <volkovsws@gmail.com>
2021-03-09 18:11:20 +0300

    add parameters, validation and some minor changes

---
+++ README.md
@@ -0,0 +1,11 @@
+## It's just CLI ticker parser 
+
+This script parse tickers from https://finviz.com/screener.ashx according to your requirements and sending to stdout.
+
+Examples:
+
+`python3 parser.py > tee tickers.txt > some another place`
+
+## CLI help end args
+
+`python3 parser.py -h` or `python3 parser.py --help` 
---
+++ parser.py
@@ -0,0 +1,130 @@
+import requests
+import argparse
+import validators
+import sys
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+
+class b_colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def validate_finviz_url(url):
+    parsed_uri = urlparse(url)
+    if parsed_uri.hostname == 'finviz.com':
+        return True
+    else:
+        return False
+
+
+def get_commandline_args():
+    parser = argparse.ArgumentParser(description='This script parse shares tickers from finviz.com')
+
+    parser.add_argument('-c', '--cheap',
+                        help='Parse only cheap shares (Under 10$)',
+                        action='store_const', const='1', )
+    parser.add_argument('-e', '--expensive',
+                        help='Parse only expensive shares (Over 10$)',
+                        action='store_const', const='2', )
+    parser.add_argument('-l', '--link',
+                        help='Create custom link with custom params and get here')
+    args = parser.parse_args()
+    return args
+
+
+def get_page_html(url, params=None):
+    page_html = requests.get(url, headers=HEADERS, params=params)
+    return page_html
+
+
+def get_paging_count():
+    page_html = get_page_html(URL)
+    soup = BeautifulSoup(page_html.text, 'html.parser')
+    pages = soup.findAll('a', class_='screener-pages')
+    if pages:
+        return int(pages[-1].get_text())
+    else:
+        return 1
+
+
+def get_tickers(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    tickers_html = soup.findAll('a', class_='screener-link-primary')
+    for ticker_html in tickers_html:
+        TICKERS.append(ticker_html.getText())
+
+
+def get_shares_number_in_page(url, params=None):
+    html = get_page_html(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    shares_number = soup.findAll('a', class_='screener-link')
+    return shares_number[len(shares_number) - 10].get_text()
+
+
+def parse():
+    html = get_page_html(URL)
+    if html.status_code == 200:
+        pages_count = get_paging_count()
+        counter = 0
+        for page in range(1, pages_count + 1):
+            print(f'Parse pages {page}/{pages_count}...')
+            current_html = get_page_html(URL, params={'r': counter})
+
+            curr_shares_counter = get_shares_number_in_page(URL, params={'r': counter})
+            counter += int(curr_shares_counter) + 1
+            get_tickers(current_html.text)
+        print(TICKERS)
+    else:
+        print('Something goes wrong. Status code: ' + html.status_code)
+
+
+def main():
+    if get_commandline_args().cheap and get_commandline_args().expensive is not None:
+        print(f"{b_colors.FAIL}Warning: \nYou can\'t get cheap and expensive tickers both!{b_colors.ENDC}")
+        # From POSIX standard - https://docs.python.org/2/library/os.html#process-management
+        sys.exit(os.EX_SOFTWARE)
+    elif get_commandline_args().cheap is not None:
+        print(f"{b_colors.OKGREEN}chip_block_detected{b_colors.ENDC}")
+        global URL
+        URL = CHEAP_URL
+        parse()
+    elif get_commandline_args().expensive is not None:
+        print(f"{b_colors.OKGREEN}exp_block_detected{b_colors.ENDC}")
+        URL = EXPENSIVE_URL
+        parse()
+    elif get_commandline_args().link is not None:
+        if validators.url(get_commandline_args().link) and validate_finviz_url(get_commandline_args().link):
+            URL = get_commandline_args().link
+            parse()
+        else:
+            print(
+                f"{b_colors.FAIL}Warning: \nNot valid URL is set, please check this link and try again.{b_colors.ENDC}")
+    else:
+        print(f"{b_colors.FAIL}Warning: \nNo one params not be established, please check -h or --help {b_colors.ENDC}")
+        # From POSIX standard - https://docs.python.org/2/library/os.html#process-management
+        sys.exit(os.EX_SOFTWARE)
+
+
+if __name__ == '__main__':
+    # https://finviz.com/screener.ashx?v=111
+    URL = ''
+    CHEAP_URL = 'https://finviz.com/screener.ashx?v=111&f=geo_usa,ind_stocksonly,sh_avgvol_o300,sh_curvol_o300,sh_price_u10,ta_averagetruerange_o0.25'
+    EXPENSIVE_URL = 'https://finviz.com/screener.ashx?v=111&f=geo_usa,ind_stocksonly,sh_avgvol_o300,sh_curvol_o300,sh_price_o10,ta_averagetruerange_o1&ft=3'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+    }
+    TICKERS = []
+    shares_in_page = []
+
+    main()
